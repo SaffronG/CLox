ca5569ba73df20901db04d2ddb6d62b542aea3d1
SaffronGamer <saffrongamer167@gmail.com>
2024-08-20 20:10:21 -0600

    FInished up to chapter 15

--- chunk.h
+++ chunk.h
@@ -1,9 +1,11 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
-#include <common.h>
+#include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -11,10 +13,14 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk * chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void writeConstant(Chunk* chunk, Value value, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
